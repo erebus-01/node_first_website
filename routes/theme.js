030484776e1d80f5939c93c4a837c37d40217603
erebus-01 <phamhiep.it.epu@gmail.com>
2022-11-09 22:34:17 +0700

    add blog

--- routes/theme.js
+++ routes/theme.js
@@ -44,15 +44,6 @@ router.get('/learn', async (req, res, next) => {
 router.get('/measure', (req, res) => {
   res.render("./theme/HTML/Measure", {layout: 'theme/layout'})
 })
-router.get('/blog', async (req, res) => {
-  try {
-    const blogs = await Blog.find({})
-    res.render("./theme/HTML/Blog", {layout: 'theme/layout', blogs: blogs})
-    next()
-  }catch (err){
-    console.log(err)
-  }
-})
 router.get('/tags/case-study', (req, res) => {
   res.render("./theme/HTML/CaseStudy", {layout: 'theme/layout'})
 })
@@ -83,17 +74,78 @@ router.get('/learn/:id', async (req, res, next) => {
 router.get('/blog/css', (req, res) => {
   res.render("./theme/HTML/DetailCourse")
 })
-router.get('/tags/:page/', async (req, res) => {
-  const currentPage = req.params.page || 0
+router.get('/tags/:page/', async (req, res, next) => {
+  const currentPage = req.params.page || 1
   const nextPage = 18
   try {
-    const tags = await Categories.find({}).skip(currentPage * nextPage).limit(nextPage)
-    res.render("./theme/HTML/Tag", {layout: 'theme/layout', tags: tags})
+    const tags = await Categories.find().sort({"_id": 1}).skip((currentPage - 1) * nextPage).limit(nextPage).exec()
+    const count = await Categories.find({}).count()
+
+    let totalPages = Math.ceil(count / nextPage)
+    const pagePlusOne = parseInt(currentPage) + 1
+    const pageMinusOne = parseInt(currentPage) - 1
+    
+    let context = {
+      layout: 'theme/layout', 
+      tags: tags, 
+      currentPage: currentPage,
+      totalPages: totalPages,
+      pagePlusOne: pagePlusOne,
+      pageMinusOne: pageMinusOne,
+    }
+
+    res.render("./theme/HTML/Tag", context)
     next()
   }catch (err){
     console.log(err)
   }
 })
+router.get('/blogs/:page/', async (req, res, next) => {
+  const currentPage = req.params.page || 1
+  const nextPage = 12
+  try {
+    const blogs = await Blog.find().sort({"_id": -1}).skip((currentPage - 1) * nextPage).limit(nextPage).exec()
+    const count = await Blog.find({}).count()
+
+    let totalPages = Math.ceil(count / nextPage)
+    const pagePlusOne = parseInt(currentPage) + 1
+    const pageMinusOne = parseInt(currentPage) - 1
+    
+    let context = {
+      layout: 'theme/layout', 
+      blogs: blogs, 
+      currentPage: currentPage,
+      totalPages: totalPages,
+      pagePlusOne: pagePlusOne,
+      pageMinusOne: pageMinusOne,
+    }
+    res.render("./theme/HTML/Blog", context)
+    next()
+  }catch (err){
+    console.log(err)
+  }
+})
+// router.get('/blog/:id', async (req, res, next) => {
+//   let id = req.params.id;
+//   try{
+//     Blog.findById(id, (error, blog) => {
+//       if(error) {
+//         res.redirect('/learn')
+//       }
+//       else{
+//         if(blog == null) {
+//           res.redirect('/learn')
+//         }
+//         else{
+//           res.render('./theme/HTML/DetailBlog', { blog: blog })
+//         }
+//       }
+//     })
+//   }
+//   catch(error) {
+//     res.status(500).json({msg: error});
+//   }
+// })
 router.get('/login', (req, res) => {
   res.render("./theme/HTML/Login", {layout: 'theme/layout'})
 })
